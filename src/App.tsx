5abd4338b32ae83876e5c8ba66eb4903ab259c15
rizalyoga <ach.rizal.yogaswara@gmail.com>
2023-02-05 12:12:30 +0700

    change calculation system

--- src/App.tsx
+++ src/App.tsx
@@ -13,124 +13,148 @@ function App() {
   const [moneys, setMoneys] = useState<Money[]>(nominal);
   const [result, setResult] = useState<number>(0);
   const [firstValue, setFirstValue] = useState<number>(0);
-  const [displayCounter, setDisplayCounter] = useState<string>("0");
+  const [displayCounter, setDisplayCounter] = useState<string[]>([]);
 
   const inputMoney = (nominal: string): void => {
     if (firstValue == 0) {
       setFirstValue(+nominal);
-      setDisplayCounter(nominal);
+      setDisplayCounter([...displayCounter, nominal]);
     } else if (
-      displayCounter.charAt(displayCounter.length - 2) == "+" ||
-      displayCounter.charAt(displayCounter.length - 2) == "-"
+      displayCounter[displayCounter.length - 1] == " + " ||
+      displayCounter[displayCounter.length - 1] == " - "
     ) {
-      setDisplayCounter(displayCounter.concat(nominal));
+      setDisplayCounter([...displayCounter, nominal]);
     }
   };
 
   const plus = (): void => {
+    const plusOperator: string = " + ";
+
     if (
       firstValue != 0 &&
-      displayCounter.charAt(displayCounter.length - 2) == "-"
+      displayCounter[displayCounter.length - 1].includes("-")
     ) {
-      setDisplayCounter(displayCounter.slice(0, -3).concat(" + "));
+      setDisplayCounter([...displayCounter.slice(0, -1), plusOperator]);
     } else if (
       firstValue != 0 &&
-      displayCounter.charAt(displayCounter.length - 2) != "+" &&
-      displayCounter.charAt(displayCounter.length - 2) != "-"
+      displayCounter[displayCounter.length - 1] != " + " &&
+      displayCounter[displayCounter.length - 1] != " - "
     ) {
-      setDisplayCounter(displayCounter.concat(" + "));
+      setDisplayCounter([...displayCounter, plusOperator]);
     }
   };
 
   const minus = (): void => {
+    const minusOperator: string = " - ";
+
     if (
       firstValue != 0 &&
-      displayCounter.charAt(displayCounter.length - 2) == "+"
+      displayCounter[displayCounter.length - 1].includes("+")
     ) {
-      setDisplayCounter(displayCounter.slice(0, -3).concat(" - "));
+      setDisplayCounter([...displayCounter.slice(0, -1), minusOperator]);
     } else if (
       firstValue != 0 &&
-      displayCounter.charAt(displayCounter.length - 2) != "+" &&
-      displayCounter.charAt(displayCounter.length - 2) != "-"
+      displayCounter[displayCounter.length - 1] != " + " &&
+      displayCounter[displayCounter.length - 1] != " - "
     ) {
-      setDisplayCounter(displayCounter.concat(" - "));
+      setDisplayCounter([...displayCounter, minusOperator]);
     }
   };
 
   const countResult = async () => {
-    setResult(evaluate(displayCounter));
+    if (displayCounter.length == 0) {
+      setResult(0);
+    } else {
+      setResult(evaluate(displayCounter.join("")));
+    }
   };
 
   const clear = (): void => {
     setFirstValue(0);
-    setDisplayCounter("0");
+    setDisplayCounter([]);
     setResult(0);
   };
 
-  const deleteValue = (): void => {};
+  const deleteValue = (): void => {
+    if (
+      displayCounter[displayCounter.length - 1] == " - " ||
+      displayCounter[displayCounter.length - 1] == " + "
+    ) {
+      setDisplayCounter(displayCounter.slice(0, -3));
+    } else {
+      setDisplayCounter(displayCounter.slice(0, -2));
+    }
+  };
 
   useEffect(() => {
+    if (displayCounter.length == 0) {
+      setFirstValue(0);
+    }
+
     countResult();
   }, [displayCounter]);
 
   return (
     <div className="main-container">
       <div className="inner-wrapper">
-        <div className="display min-h-[250px] bg-red-600 flex flex-col">
-          <div className="min-h-[120px] bg-slate-400 flex items-center p-3">
+        <div className="display min-h-[250px]flex flex-col">
+          <div className="min-h-[120px] bg-slate-300 flex items-center p-3 rounded-t-xl mt-3">
             <h1 className="text-2xl font-bold">{displayCounter}</h1>
           </div>
-          <div className="min-h-[130px] bg-red-300 flex justify-center items-center p-3">
+          <div className="min-h-[130px] bg-pink-300 flex justify-center items-center p-3 rounded-b-xl">
             <h1 className="text-2xl font-bold">{rupiah(+result)}</h1>
           </div>
         </div>
-        <div className="money-list mt-4 mb-[90px]  min-h-[200px] gap-2 grid lg:grid-cols-5 md:grid-cols-3 max-sm:grid-cols-2">
+        <div className="money-list mt-4 mb-[95px] min-h-[200px] gap-2 grid lg:grid-cols-5 md:grid-cols-3 max-sm:grid-cols-2">
           {moneys.map((money) => (
             <div
-              className="money-button flex flex-col bg-blue-300 cursor-pointer"
+              className="money-button flex flex-col bg-blue-300 cursor-pointer active:bg-pink-400"
               key={money.nominal}
               title={`${money.nominal} rupiah`}
               onClick={() => inputMoney(money.nominal)}
             >
-              <div className="image-money object-fill">
+              <div className="image-money object-cover">
                 <img
                   className="h-[250px] w-full object-cover"
                   src={money.image}
                   alt="nominal-image"
                 />
               </div>
-              <span className="text-center py-2">{money.nominal}</span>
+              <span className="text-center py-2">{rupiah(+money.nominal)}</span>
             </div>
           ))}
         </div>
-        <footer className="fixed bottom-0 w-full min-h-[80px] bg-violet-300 flex justify-center items-center gap-4  rounded-t-xl">
-          <span
-            className="text-2xl bg-slate-300 px-5 py-2 rounded-full cursor-pointer"
+        <footer className="fixed bottom-0 w-full min-h-[80px] bg-blue-300 flex justify-center items-center gap-4  rounded-t-xl">
+          <button
+            className="text-2xl bg-slate-300 px-5 py-2 rounded-full cursor-pointer active:bg-pink-400"
             onClick={plus}
           >
             +
-          </span>
-          <span
-            className="text-2xl bg-slate-300 px-5 py-2 rounded-full cursor-pointer"
+          </button>
+          <button
+            className="text-2xl bg-slate-300 px-5 py-2 rounded-full cursor-pointer active:bg-pink-400"
             onClick={minus}
           >
             -
-          </span>
-          <span className="text-2xl bg-slate-300 px-5 py-2 rounded-full cursor-pointer">
-            D
-          </span>
-          <span
-            className="text-2xl bg-slate-300 px-5 py-2 rounded-full cursor-pointer"
+          </button>
+          <button
+            className="text-2xl bg-slate-300 px-5 py-2 rounded-full cursor-pointer active:bg-pink-400"
             onClick={countResult}
           >
             =
-          </span>
-          <span
-            className="text-2xl bg-slate-300 px-5 py-2 rounded-full cursor-pointer"
+          </button>
+          <button
+            className="text-xl bg-slate-300 px-5 py-2 rounded-full cursor-pointer active:bg-pink-400"
+            onClick={deleteValue}
+          >
+            del
+          </button>
+          <button
+            className="text-xl bg-slate-300 px-5 py-2 rounded-full cursor-pointer active:bg-pink-400"
             onClick={clear}
           >
             AC
-          </span>
+          </button>
         </footer>
       </div>
     </div>
