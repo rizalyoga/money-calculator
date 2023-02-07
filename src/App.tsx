8c95021b0460833a4f4a4204c2fc4fe46dfdf7f2
rizalyoga <ach.rizal.yogaswara@gmail.com>
2023-02-07 22:50:25 +0700

    set new style & split component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from "react";
-import nominal from "./data/nominal.json";
 import "./App.css";
+import nominal from "./data/nominal.json";
 import { evaluate } from "mathjs";
 import { rupiah } from "./helper/formatRupiah";
+
+// Components
 import { showAlert } from "./components/alert/Alert";
+import Display from "./components/display/Display";
+import Footer from "./components/footer/Footer";
+import Card from "./components/card/Card";
+import Modal from "./components/modal/Modal";
 
 interface Money {
   nominal: string;
@@ -15,6 +21,8 @@ function App() {
   const [result, setResult] = useState<number>(0);
   const [firstValue, setFirstValue] = useState<number>(0);
   const [displayCounter, setDisplayCounter] = useState<string[]>([]);
+  const [isShowModal, setIsShowModal] = useState<boolean>(false);
+  const [listImageMoney, setListImageMoney] = useState<string[]>([]);
 
   const inputMoney = (nominal: string): void => {
     if (firstValue == 0) {
@@ -89,6 +97,10 @@ function App() {
     }
   };
 
+  const showModal = () => {
+    setIsShowModal((show) => !show);
+  };
+
   useEffect(() => {
     if (displayCounter.length == 0) {
       setFirstValue(0);
@@ -98,71 +110,36 @@ function App() {
   }, [displayCounter]);
 
   return (
-    <div className="main-container">
-      <div className="inner-wrapper">
-        <div className="display min-h-[250px]flex flex-col pt-3.5">
-          <div className="min-h-[120px] bg-slate-300 flex items-center p-3 rounded-t-xl">
-            <h1 className="text-2xl text-slate-700 font-bold">
-              {displayCounter}
-            </h1>
-          </div>
-          <div className="min-h-[80px] bg-pink-300 flex justify-center items-center p-3 rounded-b-xl">
-            <h1 className="text-2xl font-bold">{rupiah(+result)}</h1>
+    <>
+      <div className="main-container bg-gradient-to-br from-white via-cyan-100 to-pink-100 ">
+        <div className="inner-wrapper">
+          <Display displayCounter={displayCounter} result={result} />
+          <div className="money-list mt-4 pb-[95px] min-h-[200px] gap-2 grid lg:grid-cols-5 md:grid-cols-3 max-[768px]:grid-cols-3 max-sm:grid-cols-2 max-[322px]:grid-cols-1">
+            {moneys.map((money) => (
+              <Card
+                image={money.image}
+                nominal={money.nominal}
+                inputMoney={inputMoney}
+                key={money.nominal}
+              />
+            ))}
           </div>
         </div>
-        <div className="money-list mt-4 pb-[95px] min-h-[200px] gap-2 grid lg:grid-cols-5 md:grid-cols-3 max-sm:grid-cols-2">
-          {moneys.map((money) => (
-            <div
-              className="money-button flex flex-col bg-blue-300 cursor-pointer active:bg-pink-400 rounded-md"
-              key={money.nominal}
-              title={`${money.nominal} rupiah`}
-              onClick={() => inputMoney(money.nominal)}
-            >
-              <div className="image-money object-cover">
-                <img
-                  className="h-[250px] w-full object-cover rounded-t-md"
-                  src={money.image}
-                  alt="nominal-image"
-                />
-              </div>
-              <span className="text-center py-2">{rupiah(+money.nominal)}</span>
-            </div>
-          ))}
-        </div>
+        <Footer
+          plus={plus}
+          minus={minus}
+          countResult={countResult}
+          deleteValue={deleteValue}
+          clear={clear}
+          showModal={showModal}
+        />
       </div>
-      <footer className="fixed bottom-0 w-full min-h-[80px] bg-blue-300 flex justify-center items-center gap-4  rounded-t-xl">
-        <button
-          className="text-2xl bg-slate-300 py-3 px-[1.35rem] rounded-full cursor-pointer active:bg-pink-400"
-          onClick={plus}
-        >
-          +
-        </button>
-        <button
-          className="text-2xl bg-slate-300 py-3 px-6 rounded-full cursor-pointer active:bg-pink-400"
-          onClick={minus}
-        >
-          -
-        </button>
-        <button
-          className="text-2xl bg-slate-300 py-3 px-5 rounded-full cursor-pointer active:bg-pink-400"
-          onClick={countResult}
-        >
-          =
-        </button>
-        <button
-          className="text-xl bg-slate-300 p-3.5 rounded-full cursor-pointer active:bg-pink-400"
-          onClick={deleteValue}
-        >
-          del
-        </button>
-        <button
-          className="text-xl bg-slate-300 px-4 py-3.5 rounded-full active:bg-pink-400 "
-          onClick={clear}
-        >
-          AC
-        </button>
-      </footer>
-    </div>
+      <Modal
+        isShowModal={isShowModal}
+        setIsShowModal={setIsShowModal}
+        result={result}
+      />
+    </>
   );
 }
 
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,29 +1,36 @@
 import { useState } from "react";
+import { rupiah } from "../../helper/formatRupiah";
 
-const Modal = () => {
-  const [showModal, setShowModal] = useState<boolean>(false);
+interface ModalProps {
+  isShowModal: boolean;
+  result: number;
+  setIsShowModal: (x: boolean) => void;
+}
 
+const Modal = ({ isShowModal, setIsShowModal, result }: ModalProps) => {
   return (
     <>
       <button
         className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
         type="button"
-        onClick={() => setShowModal(true)}
+        onClick={() => setIsShowModal(true)}
       >
         Open regular modal
       </button>
-      {showModal ? (
+      {isShowModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">Modal Title</h3>
+                <div className="flex items-start justify-between text-slate-700 p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Hasil : {rupiah(+result)}
+                  </h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                    onClick={() => setShowModal(false)}
+                    onClick={() => setIsShowModal(false)}
                   >
                     <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                       ×
@@ -45,16 +52,16 @@ const Modal = () => {
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => setShowModal(false)}
+                    onClick={() => setIsShowModal(false)}
                   >
                     Close
                   </button>
                   <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    className="bg-pink-400 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                     type="button"
-                    onClick={() => setShowModal(false)}
+                    onClick={() => setIsShowModal(false)}
                   >
-                    Save Changes
+                    OK
                   </button>
                 </div>
               </div>
