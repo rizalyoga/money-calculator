af2c3d26b9ce569b3eda328963fb19de4fd6847f
rizalyoga <ach.rizal.yogaswara@gmail.com>
2023-02-08 18:47:02 +0700

    create a function to display images based on nominal

--- src/components/display/Display.tsx
+++ src/components/display/Display.tsx
@@ -8,7 +8,7 @@ interface DisplayProps {
 const Display = ({ displayCounter, result }: DisplayProps) => {
   return (
     <div className="display min-h-[250px]flex flex-col pt-3.5">
-      <div className="min-h-[120px] flex items-center p-3 rounded-t-xl bg-slate-300 backdrop-blur-md">
+      <div className="min-h-[120px] flex items-center p-3 rounded-t-xl bg-gradient-to-r from-slate-100 to-white backdrop-blur-md">
         <h1 className="text-2xl text-slate-700 font-bold">{displayCounter}</h1>
       </div>
       <div className="min-h-[80px] bg-pink-300 flex justify-center items-center p-3 rounded-b-xl">
