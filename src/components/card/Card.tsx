ccca9b7da5924baba654207fb4ad4daacb3957f5
rizalyoga <ach.rizal.yogaswara@gmail.com>
2023-07-13 16:53:57 +0700

    add sound effect

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -4,6 +4,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import useSound from "use-sound";
 
+// Image
 import seratusRibu from "../../assets/pecahan-uang/100000.webp";
 import limaPuluhRibu from "../../assets/pecahan-uang/50000.webp";
 import duaPuluhRibu from "../../assets/pecahan-uang/20000.webp";
@@ -15,7 +16,18 @@ import limaRatus from "../../assets/pecahan-uang/500.webp";
 import duaRatus from "../../assets/pecahan-uang/200.webp";
 import seratus from "../../assets/pecahan-uang/100.webp";
 
+// Audio
 import tapSoundEffect from "../../assets/audio/klick-effect.mp3";
+import play100rp from "../../assets/audio/100.mp3";
+import play200rp from "../../assets/audio/200.mp3";
+import play500rp from "../../assets/audio/500.mp3";
+import play1000rp from "../../assets/audio/1000.mp3";
+import play2000rp from "../../assets/audio/2000.mp3";
+import play5000rp from "../../assets/audio/5000.mp3";
+import play10000rp from "../../assets/audio/10000.mp3";
+import play20000rp from "../../assets/audio/20000.mp3";
+import play50000rp from "../../assets/audio/50000.mp3";
+import play100000rp from "../../assets/audio/100000.mp3";
 
 interface CardProps {
   nominal: string;
@@ -24,7 +36,16 @@ interface CardProps {
 }
 
 const Card = ({ nominal, inputMoney }: CardProps) => {
-  const [play] = useSound(tapSoundEffect);
+  const [play100] = useSound(play100rp);
+  const [play200] = useSound(play200rp);
+  const [play500] = useSound(play500rp);
+  const [play1000] = useSound(play1000rp);
+  const [play2000] = useSound(play2000rp);
+  const [play5000] = useSound(play5000rp);
+  const [play10000] = useSound(play10000rp);
+  const [play20000] = useSound(play20000rp);
+  const [play50000] = useSound(play50000rp);
+  const [play100000] = useSound(play100000rp);
 
   const setImageMoney = (nominal: number) => {
     switch (nominal) {
@@ -50,6 +71,30 @@ const Card = ({ nominal, inputMoney }: CardProps) => {
         return seratus;
       default:
         break;
+    }
+  };
+
+  const playSoundEffect = (nominal: number) => {
+    if (nominal === 100000) {
+      play100000();
+    } else if (nominal === 50000) {
+      play50000();
+    } else if (nominal === 20000) {
+      play20000();
+    } else if (nominal === 10000) {
+      play10000();
+    } else if (nominal === 5000) {
+      play5000();
+    } else if (nominal === 2000) {
+      play2000();
+    } else if (nominal === 1000) {
+      play1000();
+    } else if (nominal === 500) {
+      play500();
+    } else if (nominal === 200) {
+      play200();
+    } else if (nominal === 100) {
+      play100();
     }
   };
 
@@ -96,7 +141,7 @@ const Card = ({ nominal, inputMoney }: CardProps) => {
       className="money-button flex flex-col cursor-pointer bg-gradient-to-br from-[#FC93B4] to bg-[#D27889] active:bg-gradient-to-r active:bg-[#FBD0D7] rounded-3xl neumorphism-shadow focus:outline-none focus:ring"
       title={`${nominal} rupiah`}
       onClick={() => {
-        inputMoney(nominal), play();
+        inputMoney(nominal), playSoundEffect(+nominal);
       }}
     >
       <div className="image-money object-cover ">
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,16 @@
 import useSound from "use-sound";
+
+// Icon
 import clearIcon from "../../assets/icon/clear.png";
 import delIcon from "../../assets/icon/del-arrow.png";
-import resultSoundEffect from "../../assets/audio/result.mp3";
+import minusIcon from "../../assets/icon/minus.png";
+import plusIcon from "../../assets/icon/plus.png";
+import equalIcon from "../../assets/icon/equal.png";
+
+//  Audio
+import resultSoundEffect from "../../assets/audio/Sama_Dengan.mp3";
+import plusSoundEffect from "../../assets/audio/ditambah.mp3";
+import minusSoundEffect from "../../assets/audio/dikurangi.mp3";
 import clickSoundEffect from "../../assets/audio/klick-effect.mp3";
 
 interface FooterProps {
@@ -23,32 +32,34 @@ const Footer = ({
 }: FooterProps) => {
   const [playResult] = useSound(resultSoundEffect);
   const [playClick] = useSound(clickSoundEffect);
+  const [playPlus] = useSound(plusSoundEffect);
+  const [playMinus] = useSound(minusSoundEffect);
 
   return (
     <footer className="fixed bottom-0 w-full min-h-[80px] bg-gradient-to-r bg-[#EE8997] flex justify-center items-center gap-4 max-[362px]:gap-1 rounded-t-xl ">
       <button
-        className="text-4xl text-[#EE8997] font-bold bg-white py-2 px-[1.1rem] rounded-full active:bg-red-300 active:text-white neumorphism-shadow-button"
+        className="text-4xl text-[#EE8997] font-bold bg-white py-4 px-[1rem] rounded-full active:bg-red-300 active:text-white neumorphism-shadow-button"
         onClick={() => {
-          plus(), playClick();
+          plus(), playPlus();
         }}
       >
-        +
+        <img src={plusIcon} alt="plus-icon" />
       </button>
       <button
-        className="text-4xl text-[#EE8997] font-bold bg-white py-2 px-[1.4rem] rounded-full active:bg-red-300 active:text-white neumorphism-shadow-button"
+        className="text-4xl text-[#EE8997] font-bold bg-white py-4 px-4 rounded-full active:bg-red-300 active:text-white neumorphism-shadow-button"
         onClick={() => {
-          minus(), playClick();
+          minus(), playMinus();
         }}
       >
-        -
+        <img src={minusIcon} alt="minus-icon" />
       </button>
       <button
-        className="font-bold text-4xl bg-[#FFBD66] text-white py-[0.5rem] px-[1.1rem] rounded-full active:bg-red-300 active:text-white neumorphism-shadow-button focus:outline-none focus:ring"
+        className="font-bold text-4xl bg-[#FFBD66] text-white py-4 px-4 rounded-full active:bg-red-300 active:text-white neumorphism-shadow-button focus:outline-none focus:ring"
         onClick={() => {
           showModal(), playResult();
         }}
       >
-        =
+        <img src={equalIcon} alt="equal-icon" className="w-[28px]" />
       </button>
       <button
         className="text-xl font-bold bg-white text-[#FFBD66] p-3.5 rounded-full active:bg-red-300 active:text-white neumorphism-shadow-button"
